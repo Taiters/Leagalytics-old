1bf7bc2448c16eaa7b7a65e4f14ef257d52f802d
Daniel Tait <dantait91@gmail.com>
2014-10-15 01:05:09 +0100

    Fixed typo (mv -> cp) and added nginx reload to install.sh

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -1,7 +1,9 @@
-sudo mv /vagrant/vagrant/nginx-host /etc/nginx/sites-available/vagrant
+sudo cp /vagrant/vagrant/nginx-host /etc/nginx/sites-available/vagrant
 
 cd /vagrant
 
 composer install
 npm install
 bower install
+
+sudo service nginx reload
